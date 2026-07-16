99535f1ae6eed050c50074bca528c5603072b634
shriyantrafx <shriyantrasoni2@gmail.com>
2026-07-16 16:58:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,15 +2,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-PASTE YOUR FIREBASE CONFIG HERE// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
   apiKey: "AIzaSyASyQefLiSnF3vooHpQ1fzzPLBI229CASM",
   authDomain: "tradersfx-community.firebaseapp.com",
   projectId: "tradersfx-community",
@@ -19,13 +10,8 @@ const firebaseConfig = {
   appId: "1:166627997744:web:7b6bcdc4296598a37655f5",
   measurementId: "G-6SL8BEQZRM"
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-};
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
+export { auth };
