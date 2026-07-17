c8351b29f15582571f92c3b110539675ea8adf7a
shriyantrafx <shriyantrasoni2@gmail.com>
2026-07-17 14:24:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,45 @@ candles.shift();
 }
 
 }
+// ===== Draw Candles =====
+
+function drawCandles(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+candles.forEach(candle=>{
+
+const green = candle.close > candle.open;
+
+const color = green ? "#22c55e" : "#ef4444";
+
+ctx.strokeStyle = color;
+ctx.fillStyle = color;
+
+ctx.shadowBlur = 20;
+ctx.shadowColor = color;
+
+// Wick
+ctx.beginPath();
+ctx.moveTo(candle.x,candle.high);
+ctx.lineTo(candle.x,candle.low);
+ctx.stroke();
+
+// Body
+ctx.fillRect(
+    candle.x-5,
+    candle.open,
+    10,
+    candle.close-candle.open
+);
+
+// Move candle
+candle.x -= 2;
+
+});
+
+ctx.shadowBlur = 0;
+
+}
+
+setInterval(createCandle,350);
