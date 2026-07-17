08d8702641b47d58599e96383ff6859271ea4abc
shriyantrafx <shriyantrasoni2@gmail.com>
2026-07-17 14:23:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+// ===== TradersFX Background Engine =====
 
+const bg = document.querySelector(".background");
+
+const canvas = document.createElement("canvas");
+
+bg.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas(){
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+}
+
+resizeCanvas();
+
+window.addEventListener("resize",resizeCanvas);
+
+
+// ===== Candle Data =====
+
+const candles=[];
+
+function random(min,max){
+
+return Math.random()*(max-min)+min;
+
+}
+
+function createCandle(){
+
+const open=random(120,500);
+
+const close=open+random(-70,70);
+
+candles.push({
+
+x:canvas.width+40,
+
+open,
+
+close,
+
+high:Math.max(open,close)+random(10,35),
+
+low:Math.min(open,close)-random(10,35),
+
+});
+
+if(candles.length>70){
+
+candles.shift();
+
+}
+
+}
